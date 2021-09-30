4d782234729e4cd8cbe849240c12b87ffd7d83ff
Daniel Araujo De Lucena <daniel.lucena@SP-MBZ3697.local>
2021-09-30 15:50:12 -0300

    Criação do diretório de componentes, ajustes em App.js e Button.js

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text>Calculadora</Text>
+    </SafeAreaView>
+  );
+}
+
---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Dimensions,
+    TouchableHighlight,
+  } from 'react-native';
+
+const styles = StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#88*'
+    }
+})
+
+export default props => {
+    return(
+        <TouchableHighlight onPress={props.onClick}>
+            <Text style={styles.button}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
